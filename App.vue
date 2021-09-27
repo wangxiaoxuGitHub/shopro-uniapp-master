049961d7b9f1d8265c4cf05778f40188fc5702eb
wangxiaoxu <wangxiaoxu_mail@163.com>
2021-09-27 08:26:09 +0800

    init

---
+++ App.vue
@@ -0,0 +1,14 @@
+<script>
+	import {
+		init
+	} from "@/shopro";
+	export default {
+		onLaunch(options) {
+			init(options);
+		}
+	};
+</script>
+
+<style lang="scss">
+	@import 'static/style/index.scss';
+</style>
